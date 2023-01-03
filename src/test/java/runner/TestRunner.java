672e9a082d89cb8bb6bfe049cd620ab1046e0151
Kaciro <121603514+Kaciro@users.noreply.github.com>
2023-01-03 13:41:12 +0100

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//         plugin ={"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+        plugin ={"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         features = "src/test/resources/features",
         glue = "steps",
         tags = "@Demo"
